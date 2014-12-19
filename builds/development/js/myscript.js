13d809fcd3d71725679003425bc2d92cec06c07d
Ray Villalobos <ray@planetoftheweb.com>
2014-12-19 15:34:06 -0800

    03_06 Creating a Full Screen Carousel

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -4,6 +4,25 @@ $(function() {
 
   var topoffset = 50; //variable for menu height
   var slideqty = $('#featured .item').length;
+  var wheight = $(window).height(); //get the height of the window
+  
+  $('.fullheight').css('height', wheight); //set to window tallness  
+
+
+  //replace IMG inside carousels with a background image
+  $('#featured .item img').each(function() {
+    var imgSrc = $(this).attr('src');
+    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+    $(this).remove();
+  });
+
+  //adjust height of .fullheight elements on window resize
+  $(window).resize(function() {
+    wheight = $(window).height(); //get the height of the window
+    $('.fullheight').css('height', wheight); //set to window tallness  
+  });
+
+
 
   //Activate Scrollspy
   $('body').scrollspy({
