5a54cf4fe3a1cdcff07204d46535c5c93f25e4e1
Ray Villalobos <ray@planetoftheweb.com>
2014-12-19 11:12:18 -0800

    03_01 Using Scrollspy with Single Page Sites

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,3 +1,36 @@
-$('.carousel').carousel({
-  interval: false
+$(function() {
+
+  "use strict";
+
+  var topoffset = 50; //variable for menu height
+
+  //Activate Scrollspy
+  $('body').scrollspy({
+    target: 'header .navbar',
+    offset: topoffset
+  });
+
+  var hash = $(this).find('li.active a').attr('href');
+  if(hash !== '#featured') {
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+
+
+  // Add an inbody class to nav when scrollspy event fires
+  $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+    var hash = $(this).find('li.active a').attr('href');
+    if(hash !== '#featured') {
+      $('header nav').addClass('inbody');
+    } else {
+      $('header nav').removeClass('inbody');
+    }
+  });
+
+
+  $('.carousel').carousel({
+    interval: false
+  });
+
 });
